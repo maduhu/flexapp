f8da0891c93d2ee99ba8574b81c9f7a2cc3a9fb6
Greg Plumbly <greg@portablepixels.com>
2013-12-03 22:07:42 +0000

    Added form to email script

---
+++ sendsupportemail.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+
+if(isset($_POST['email']) && isset($_POST['feedback'])) {
+	
+	require("class.phpmailer-lite.php");
+	$mail = new PHPMailerLite();
+	$mail->SetFrom('feedback@portablepixels.com', 'Feedback');
+	$mail->AddAddress("support@portablepixels.com", "Phonics App Website Feedback");
+
+//	$mail->AddAttachment($attachment_name);
+	
+	$mail->Subject = "Feedback from Phonics App Contact Form";
+
+	
+	$content  = "Name: " . trim($_POST['name']) . "\n";
+	$content .= "Email: " . trim($_POST['email']) . "\n";
+	$content .= "Subject: " . trim($_POST['subject']) . "\n";
+	$content .= "Feedback: " . trim($_POST['feedback']) . "\n";
+	$mail->Body = $content;
+	
+	if(empty($_POST['name']) && empty($_POST['email']) && empty($_POST['feedback']) ) {
+		echo "Message wasn't sent because the fields were empty.";
+		exit;
+	}
+
+	if(!$mail->Send())
+	{
+		header("HTTP/1.1 500 Message could not be sent.");
+		echo "Mail error:$mail->ErrorInfo";
+	  	exit;
+	}
+
+	echo "Message has been sent";
+	exit;
+}
+
+header('HTTP/1.1 403 Forbidden');
+echo "Missing information.";
+exit;
+
+?>
